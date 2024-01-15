e8c77a41e0ed2b7611733c2adb912db679346217
Richard Acquaye <racquaye89@gmail.com>
2024-01-15 19:48:51 +0000

    refactor: Improve function readability

--- 07_Birthday-Day-Forcast/script.js
+++ 07_Birthday-Day-Forcast/script.js
@@ -2,23 +2,22 @@
 
 const calcBirthdayDay = (date, month, dayName) => {
   const curYear = new Date().getFullYear();
+  const parseDay = dayName[0].toUpperCase() + dayName.slice(1, 3); // Converts to short-form day
   const locale = navigator.language;
-  const parseDay = dayName[0].toUpperCase() + dayName.slice(1, 3);
   const fullDateOptions = {
     dateStyle: "full",
   };
 
   for (let i = curYear; i <= curYear + 50; i++) {
     const refDate = new Date(i, month - 1, date);
+    const forcastDate = new Intl.DateTimeFormat(locale, fullDateOptions).format(
+      refDate.setFullYear(i)
+    );
 
-    if (String(refDate).startsWith(parseDay)) {
-      console.log(
-        new Intl.DateTimeFormat(locale, fullDateOptions).format(
-          refDate.setFullYear(i)
-        )
-      );
+    if (refDate.toDateString().startsWith(parseDay)) {
+      console.log(forcastDate);
     }
   }
 };
 
-calcBirthdayDay(9, 8, "Sunday");
+calcBirthdayDay(9, 8, "Saturday");
