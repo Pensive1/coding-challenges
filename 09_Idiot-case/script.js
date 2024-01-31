4e83bbc23cd3ba6463f2a0422cf375b942f00bd2
Richard Acquaye <racquaye89@gmail.com>
2024-01-31 00:25:48 +0000

    Add Idiot-case challenge

---
+++ 09_Idiot-case/script.js
@@ -0,0 +1,54 @@
+"use strict";
+
+// NOTE: I'm sure this function will be more efficient with regex.
+// For now, do this to the best of your ability
+
+const idiotCase = (str) => {
+  const strArr = str.toLowerCase().split("");
+  const output = [];
+
+  for (let i = 0; i < strArr.length; i++) {
+    let prevLetr = output[i - 1];
+    let charBeforeSpace = output[i - 2];
+    let charBeforeSpaceUpper =
+      prevLetr === " " && charBeforeSpace === charBeforeSpace.toUpperCase();
+    let charBeforeSpaceLower =
+      prevLetr === " " && charBeforeSpace === charBeforeSpace.toLowerCase();
+
+    //Push empty spaces
+    if (strArr[i] === " ") {
+      output.push(strArr[i]);
+      continue;
+    }
+
+    // Capitalise first strArr
+    if (i === 0) {
+      output.push(strArr[i].toUpperCase());
+      continue;
+    }
+
+    // If prev strArr exists, is not empty and  is uppercase -> Lower case it
+    if (
+      (prevLetr !== " " && prevLetr === prevLetr.toUpperCase()) ||
+      charBeforeSpaceUpper
+    ) {
+      output.push(strArr[i].toLowerCase());
+      continue;
+    }
+
+    // If prev strArr exists, is not empty and  is lowercase -> capitalise it
+    if (
+      (prevLetr !== " " && prevLetr === prevLetr.toLowerCase()) ||
+      charBeforeSpaceLower
+    ) {
+      output.push(strArr[i].toUpperCase());
+      continue;
+    }
+  }
+  return output.join("");
+
+  return jumblestrArr(strArr);
+};
+
+console.log(idiotCase("This is my house"));
+console.log(idiotCase("And this is my second house"));
