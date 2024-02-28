68b6e80e1c95b866a5c6a1eee79389b50434594a
Richard Acquaye <racquaye89@gmail.com>
2024-02-28 13:30:28 +0000

    Complete human, cat and dog age conversion challenge

---
+++ 13_Human-cat-dog-years/script.js
@@ -0,0 +1,30 @@
+const calculateYears = (humanYears) => {
+  if (humanYears < 1) return null;
+
+  let catYears = 0;
+  let dogYears = 0;
+
+  for (let i = 1; i <= humanYears; i++) {
+    if (i === 1) {
+      catYears += 15;
+      dogYears += 15;
+    }
+
+    if (i === 2) {
+      catYears += 9;
+      dogYears += 9;
+    }
+
+    if (i > 2) {
+      catYears += 4;
+      dogYears += 5;
+    }
+  }
+
+  return [humanYears, catYears, dogYears];
+};
+
+console.log(calculateYears(1));
+console.log(calculateYears(2));
+console.log(calculateYears(10));
+console.log(calculateYears(0));
