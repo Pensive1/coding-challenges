cca78735dd729f9221cd3eb8abf7d3071012a823
Richard Acquaye <racquaye89@gmail.com>
2024-03-27 14:20:21 +0000

    refactor: Add instructions (delete duplicates by n)

--- 04_Delete-duplicates-by-n/script.js
+++ 04_Delete-duplicates-by-n/script.js
@@ -1,26 +1,30 @@
 function deleteNth(arr, n) {
-  const tempArr = [];
+  // const tempArr = [];
 
-  for (let i = 0; i < arr.length; i++) {
-    let currentValueCount = tempArr.filter((num) => num === arr[i]).length;
+  // ----- OLD CODE -----
+  // for (let i = 0; i < arr.length; i++) {
+  //   let currentValueCount = tempArr.filter((num) => num === arr[i]).length;
+
+  //   if (
+  //     !tempArr.includes(arr[i]) ||
+  //     (tempArr.includes(arr[i]) && currentValueCount < n)
+  //   ) {
+  //     tempArr.push(arr[i]);
+  //   }
+  // }
+
+  return arr.reduce((finalArr, cur) => {
+    let currentValueCount = finalArr.filter((num) => num === cur).length;
 
     if (
-      !tempArr.includes(arr[i]) ||
-      (tempArr.includes(arr[i]) && currentValueCount < n)
+      !finalArr.includes(cur) ||
+      (finalArr.includes(cur) && currentValueCount < n)
     ) {
-      tempArr.push(arr[i]);
+      finalArr.push(cur);
     }
-  }
-  return tempArr;
+    return finalArr;
+  }, []);
 }
 
-console.log(
-  deleteNth(
-    [
-      23, 7, 43, 43, 7, 41, 7, 26, 22, 7, 41, 23, 7, 26, 7, 22, 22, 41, 41, 43,
-      7, 23, 23, 22, 10, 43, 43, 41, 10, 26, 41, 10, 7, 23, 41, 23, 43, 22, 23,
-      10, 22, 10, 7, 23, 22,
-    ],
-    2
-  )
-);
+console.log(deleteNth([23, 7, 43, 43, 7, 41, 7, 26, 22, 7, 41, 7], 2));
+console.log(deleteNth([1, 2, 5, 7, 8, 6, 4, 5, 5, 1, 1, 2, 3, 3, 3], 1));
